122fd7cd40e12c5be4ff9ebce7031e9a08a00b4d
Rinku <kukadiyarinku@gmail.com>
2022-06-30 16:52:58 -0700

    Static JSON Data

---
+++ Data.js
@@ -0,0 +1,7 @@
+const data = [
+    { name: 'Rinku', email: 'kukadiyarinku@gmail.com' },
+    { name: 'Vishal', email: 'kukadiyavishal@gmail.com' },
+    { name: 'Jay', email: 'kukadiyajay@gmail.com' }
+];
+
+module.exports = data;
--- Index.js
+++ Index.js
@@ -1,15 +1,17 @@
 const http = require('http');
+const mdata = require('./Data.js');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostName = '127.0.0.1';
+const portNo = 3060;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!.');
-
+    res.setHeader('Content-Type', 'application\json');
+    res.write(JSON.stringify(mdata));
+    res.end();
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(portNo, hostName, () => {
+    console.log(`Server running at http://${hostName}:${portNo}/`);
 });
+
