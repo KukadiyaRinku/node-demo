d1856f3695a4e62b0df3badf87b2ce6af49768a7
Rinku <kukadiyarinku@gmail.com>
2022-07-01 11:45:16 -0700

    Advance Static Page Routing

--- Index.js
+++ Index.js
@@ -4,6 +4,22 @@ const path = require('path')
 const app = express();
 const viewPath = path.join(__dirname, 'View');
 
-app.use(express.static(viewPath));
+//app.use(express.static(viewPath));
+
+app.get('', (_, resp) => {
+    resp.sendFile(`${viewPath}/Index.html`);
+});
+
+app.get('/about', (_, resp) => {
+    resp.sendFile(`${viewPath}/AboutUs.html`);
+});
+
+app.get('/contact', (_, resp) => {
+    resp.sendFile(`${viewPath}/ContactUs.html`);
+});
+
+app.get('*', (_, resp) => {
+    resp.sendFile(`${viewPath}/NotFound.html`);
+});
 
 app.listen(3060);
